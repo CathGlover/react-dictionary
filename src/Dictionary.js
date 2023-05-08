9b73f33a4e59cad85dca8c9a9aef3e3be0adcb67
CathGlover <117387100+CathGlover@users.noreply.github.com>
2023-05-08 12:41:52 +0100

    added url for shecodes dictionary app without axios call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
@@ -10,6 +12,11 @@ export default function Dictionary() {
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+    let url = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=edf069311acf2bebo10f4bbbc53249t3`;
+  }
+
+  function handleResponse(event) {
+    console.log(event);
   }
 
   return (
